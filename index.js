d426196a70d4fca958a1d32796a2c676c8380436
Brian Hung <brianhung@berkeley.edu>
2019-07-26 13:17:35 -0700

    rewrote app logic to reduce repetitive queries

--- index.js
+++ index.js
@@ -1,48 +1,106 @@
-function getDisplay(element) {
-    return window.getComputedStyle(element).getPropertyValue("display")
+/**
+ * Displays the editor.
+ * @return undefined
+ */
+function displayEditor(cell) {
+    if (cell.editing) {
+        cell.editor.style.display =  "none"
+        cell.render.style.display =  "block"
+        cell.editing = false
+        cell.focus()
+    } else {
+        cell.render.style.display =  "none"
+        cell.editor.style.display =  "block"
+        cell.editing = true
+        cell.editor.CodeMirror.focus()
+    }
 }
 
+/**
+ * Displays the render.
+ * @return undefined
+ */
+function displayRender(cell) {
+    const emptyText = `<p><em> empty cell </em></p>`
+    cell.render.innerHTML = markdown.render(cell.editor.CodeMirror.getValue()) || emptyText
+    displayEditor(cell)
+}
+
+/**
+ * Returns json representation of text based on ipynb format.
+ * @return json
+ */
 function saveTextAsJson() {
-
     let cells  = []
-    let editor = document.querySelectorAll(".cell-editor")
-
-    for(let e of editor) {
-        let m = e.CodeMirror
-        let c = {}
-
-        c.cell_type = m.getOption("mode")
-        c.source    = m.getValue()
-        c.editor    = getDisplay(e)
-
-        cells.push(c)
+    for(let cell of document.querySelectorAll(".cell")) {
+        let mirror = cell.editor.CodeMirror
+        let temp = {}
+        temp.cell_type = mirror.getOption("mode")
+        temp.source    = mirror.getValue()
+        temp.editing   = cell.editing
+        cells.push(temp)
     }
-
     return JSON.stringify({metadata: {}, cells: cells})
 }
 
-function initPgFromJson(json) {
-
+/**
+ * Initializes the page from json representation.
+ * @return undefined
+ */
+function initPgFromJson(body, json) {
+    let cells = json.cells
+    for(let c of cells) {
+        let o = Object.assign({}, defaultOptions, {mode: c.cell_type, value: c.source})
+        addCell(body, mirrorOptions=o, editing=c.editing)
+    }
 }
 
+/**
+ * Adds a cell to the parent parameter.
+ * @return dom element
+ */
+function addCell(parent, nextCell=null, mirrorOptions=defaultOptions, editing=true) {
+    // Create temporary element to initialize cell.
+    let temp = document.createElement("div")
+    temp.innerHTML = `<div class="cell" tabindex="0"><div class="cell-render"></div></div>`
 
+    // Insert cell into view before CodeMirror (prevents default).
+    let cell = temp.firstChild
+    parent.insertBefore(cell, nextCell)
+
+    // Create CodeMirror instance for cell.
+    let mirror = CodeMirror(cell, mirrorOptions)
+    mirror.getWrapperElement().className += " " + "cell-editor"
+
+    // Create shorthands to reference editor and render.
+    cell.editor = cell.querySelector(".cell-editor")
+    cell.render = cell.querySelector(".cell-render")
+
+    // Display editor if editing is true.
+    editing ? displayEditor(cell) : displayRender(cell)
+    return cell
+}
+
+/**
+ * Saves markdown representation of text.
+ * @return undefined
+ */
 function saveTextAsFile(value, filename) {
 
     var textFileAsBlob = new Blob([value], {
         type: "text/plain;charset=utf-8"
     });
 
-    let download = document.createElement("a");
-    download.download  = filename + ".txt";
-    download.href = window.URL.createObjectURL(textFileAsBlob);
-    download.onclick = destroyClickedElement;
-    download.style.display = "none";
-    document.body.appendChild(download);
-
-    download.click();
+    let file = document.createElement("a");
+    file.download = filename + ".txt";
+    file.href = window.URL.createObjectURL(textFileAsBlob);
+    file.onclick = destroyClickedElement;
+    file.style.display = "none";
+    file.body.appendChild(download);
+    file.click();
 }
 
-var default_options = {
+const defaultOptions = {
     mode: "gfm",
     lineWrapping: true,
     theme: "default",
@@ -55,70 +113,16 @@ var default_options = {
             if (cm.getOption("fullScreen")) {
                 cm.setOption("fullScreen", false)
             }
-        },
+        }
     }
-}
-
-var cells = document.querySelectorAll(".cell")
-for (let cell of cells) {
-    var code = CodeMirror(cell, default_options)
-    code.getWrapperElement().className += " " + "cell-editor"
 }
 
 function EventListener(body) {
-
-    function addCell(referenceCell) {
-        // Create temporary element to initialize cell.
-        let temp = document.createElement("div")
-        temp.innerHTML = `<div class="cell" tabindex="0"><div class="cell-render"></div></div>`
-
-        let cell = temp.firstChild
-        body.insertBefore(cell, referenceCell)
-
-        // Create CodeMirror instance for cell.
-        var code = CodeMirror(cell, default_options)
-        code.getWrapperElement().className += " " + "cell-editor"
-        return cell
-    }
-
-    function displayEditor(event) {
-        // Initialize temporary elements.
-        let cell = event.target.closest(".cell")
-        let editor = cell.querySelector(".cell-editor")
-        let render = cell.querySelector(".cell-render")
-
-        if (editor.style.display == "none") {
-            editor.style.display =  "block"
-            render.style.display =  "none"
-            editor.CodeMirror.focus()
-        } else {
-            editor.style.display =  "none"
-            render.style.display =  "block"
-            cell.focus()
-        }
-
-    }
-
-    function displayRender(event) {
-        // Initialize temporary elements.
-        let cell = event.target.closest(".cell")
-        let editor = cell.querySelector(".cell-editor")
-        let render = cell.querySelector(".cell-render")
-
-        const emptyText  = `<p><em> empty cell </em></p>`
-        render.innerHTML = markdown.render(editor.CodeMirror.getValue()) || emptyText
-
-        displayEditor(event)
-    }
 
     body.addEventListener("keydown",  function(event) {
         // Event target is with-in a cell element.
         let cell = event.target.closest(".cell")
         if (cell != null) {
-
-            let editor = cell.querySelector(".cell-editor")
-            let render = cell.querySelector(".cell-render")
-            let mirror = editor.CodeMirror
 
             switch(event.keyCode) {
                 case  8: // backspace
@@ -133,34 +137,35 @@ function EventListener(body) {
                 case 13: // enter
                     if (event.shiftKey) {
                         // Display render if not displayed.
-                        if (getDisplay(render) == "none") {
-                            displayRender(event)
+                        console.log(cell, cell.editing)
+                        if (cell.editing) {
+                            displayRender(cell)
                         // Focus on next cell if render is displayed.
                         } else {
-                            cell = cell.nextElementSibling
-                            if (cell == null || cell.className != "cell") {
-                                cell = addCell(cell)
+                            let next = cell.nextElementSibling
+                            if (next == null || next.className != "cell") {
+                                cell = addCell(body, next)
                             } else {
-                                cell.focus()
+                                next.focus()
                             }
                         }
                     } else {
                         // Check editor if not displayed.
-                        if (getDisplay(editor) == "none") {
+                        if (!cell.editing) {
                             // Prevent enter from creating new line.
                             event.preventDefault()
-                            displayEditor(event)
+                            displayEditor(cell)
                         }
                     }
                     break
                 case 27: // esc
-                    if (getDisplay(editor) != "none") {
+                    if (cell.editing) {
                         cell.focus()
                     }
                     break
                 case 38: // up-arrow
                     if (event.shiftKey) {
-                        if (getDisplay(render) != "none") {
+                        if (!cell.editing) {
                             let prev = cell.previousElementSibling
                             if (prev != null && prev.className == "cell") {
                                 cell.parentElement.insertBefore(cell, prev)
@@ -168,19 +173,18 @@ function EventListener(body) {
                             }
                         }
                     } else {
-                        let cursor = mirror.getCursor()
+                        let mirror  = cell.editor.CodeMirror
+                        let cursor  = mirror.getCursor()
                         let lineNum = 0
                         let charNum = 0
                         // This triggers because of a CodeMirror key event.
                         let frstLine = (cursor.line == lineNum && cursor.ch == charNum)
-                        if (getDisplay(render) != "none" || frstLine) {
+                        if (!cell.editing || frstLine) {
                             let prev = cell.previousElementSibling
                             if (prev != null && prev.className == "cell") {
-                                let editor = prev.querySelector(".cell-editor")
-                                let render = prev.querySelector(".cell-render")
-                                let mirror = editor.CodeMirror
                                 // Focus on prev editor if displayed.
-                                if (getDisplay(render) == "none") {
+                                if (prev.editing) {
+                                    let mirror = prev.editor.CodeMirror
                                     mirror.focus()
                                 } else {
                                     prev.focus()
@@ -191,7 +195,7 @@ function EventListener(body) {
                     break
                 case 40: // dw-arrow
                     if (event.shiftKey) {
-                        if (getDisplay(render) != "none") {
+                        if (!cell.editing) {
                             let next = cell.nextElementSibling
                             if (next != null && next.className == "cell") {
                                 next.parentElement.insertBefore(next, cell)
@@ -199,19 +203,18 @@ function EventListener(body) {
                             }
                         }
                     } else {
+                        let mirror  = cell.editor.CodeMirror
                         let cursor  = mirror.getCursor()
                         let lineNum = mirror.lastLine()
                         let charNum = mirror.getLine(lineNum).length
                         // This triggers because of a CodeMirror key event.
                         let lastLine = (cursor.line == lineNum && cursor.ch == charNum)
-                        if (getDisplay(render) != "none" || lastLine) {
+                        if (!cell.editing || lastLine) {
                             let next = cell.nextElementSibling
                             if (next != null && next.className == "cell") {
-                                let editor = next.querySelector(".cell-editor")
-                                let render = next.querySelector(".cell-render")
-                                let mirror = editor.CodeMirror
                                 // Focus on prev editor if displayed.
-                                if (getDisplay(render) == "none") {
+                                if (next.editing) {
+                                    let mirror = next.editor.CodeMirror
                                     mirror.focus()
                                 } else {
                                     next.focus()
@@ -221,15 +224,13 @@ function EventListener(body) {
                     }
                     break
                 case 74: // j
-                    mirror.setOption("mode", "javascript")
+                    cell.editor.CodeMirror.setOption("mode", "javascript")
                     break
                 case 77: // m
-                    mirror.setOption("mode", "gfm")
-                    mirror.refresh()
+                    cell.editor.CodeMirror.setOption("mode", "gfm")
                     break
                 case 80: // p
-                    mirror.setOption("mode", "python")
-                    mirror.refresh()
+                    cell.editor.CodeMirror.setOption("mode", "python")
                     break
             }
         // Event target is outside a cell.
@@ -239,7 +240,7 @@ function EventListener(body) {
                     if (event.shiftKey) {
                         cell = document.querySelector(".cell")
                         if (cell == null) {
-                            cell = addCell()
+                            cell = addCell(body)
                         }
                     }
                     break
@@ -251,13 +252,9 @@ function EventListener(body) {
         // Check if target is within a cell element.
         let cell = event.target.closest(".cell")
         if (cell != null) {
-
-            let editor = cell.querySelector(".cell-editor")
-            let render = cell.querySelector(".cell-render")
-
             // Check if editor is displayed.
-            if (getDisplay(editor) == "none") {
-                displayEditor(event)
+            if (cell.editing) {
+                displayEditor(cell)
             }
         // Target is outside a cell element.
         } else {
